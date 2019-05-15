4e91d50cd2452e766fd1a964c900dd9eb7ce72b9
Jordan McQuiston <jordan@jordancley.com>
2019-05-14 17:36:04 -0700

    Initial Commit(again)

---
+++ about.php
@@ -0,0 +1,28 @@
+
+<?php include('includes/header.php') ?>
+
+<div id="slider">
+    <div id="carouselExampleFade" class="carousel carousel-fade" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="/img/HNRL_outside.png" class="d-block w-100" alt="HONOR ROLL SKATE CREW">
+            </div>
+            <div class="carousel-item">
+                <img src="/img/HNRL_silly.png" class= "d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="/img/HNRL_street.png" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+
+<?php include('includes/footer.php') ?>
---
+++ includes/header.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="/assets/styles.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Honor Roll Skate Crew</title>
+</head>
+<body>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top"><!-- navbar start -->
+            <a class="navbar-brand" href="#">HNRL</a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item navbar-toggle">
+                        <a class="nav-link" href="/index.php">Home</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about.php">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/videos.php">Videos</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink"
+                           role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            The Crew
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="/crew.php">Brandon Perea</a>
+                            <a class="dropdown-item" href="/crew.php">Diamond Walker</a>
+                            <a class="dropdown-item" href="/crew.php">Hunter Collins</a>
+                            <a class="dropdown-item" href="/crew.php">Jordan McQuiston</a>
+                            <a class="dropdown-item" href="/crew.php">Tony Zane</a>
+                        </div>
+                    </li>
+                </ul>
+                <ul class="nav justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="https://www.facebook.com/thehnrl/">
+                            <img src="img/fb_icon.png" alt="Facebook">
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://www.instagram.com/thehnrl/">
+                            <img src="img/insta_icon.png" alt="Instagram">
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/contact.php">
+                            <button class="btn btn-outline-warning btn-lg my-2 my-sm-0" type="button">
+                                Contact Us!
+                            </button>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav><!-- navabar end -->
+    </div>
