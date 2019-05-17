d91ccc410eef3875e8163e2622ba7a65ce095e07
Jordan McQuiston <jordan@jordancley.com>
2019-05-16 20:41:24 -0700

    contact page structure

---
+++ includes/arrays.php
@@ -0,0 +1,84 @@
+<?php
+
+// Navigation Menu Items
+
+$images = array(
+
+    array(
+        "img"	=> "DC",
+    ),
+
+    array(
+        "img"	=> "",
+    ),
+
+    array(
+        "slug"	=> "menu.php",
+        "title"	=> "Menu"
+    ),
+
+    array(
+        "slug"	=> "contact.php",
+        "title"	=> "Contact"
+    ),
+
+);
+
+
+// Team Members
+
+$crewMembers = array(
+
+    array(
+
+        "firstName"		=> "Jordan",
+        "lastName"		=> "McQuiston",
+        "age"		    =>  34,
+        "bio"			=> "blahh blahh",
+        "img"			=> "test"
+
+    ),
+
+    array(
+
+        "firstName"		=> "Jordan",
+        "lastName"		=> "McQuiston",
+        "age"		    =>  34,
+        "bio"			=> "blahh blahh",
+        "img"			=> "test"
+
+    ),
+
+    array(
+
+        "firstName"		=> "Jordan",
+        "lastName"		=> "McQuiston",
+        "age"		    =>  34,
+        "bio"			=> "blahh blahh",
+        "img"			=> "test"
+
+    ),
+
+    array(
+
+        "firstName"		=> "Jordan",
+        "lastName"		=> "McQuiston",
+        "age"		    =>  34,
+        "bio"			=> "blahh blahh",
+        "img"			=> "test"
+
+    ),
+
+    array(
+
+        "firstName"		=> "Jordan",
+        "lastName"		=> "McQuiston",
+        "age"		    =>  34,
+        "bio"			=> "blahh blahh",
+        "img"			=> "test"
+
+    )
+
+);
+
+?>
--- includes/header.php
+++ includes/header.php
@@ -5,15 +5,19 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="/assets/styles.css" rel="stylesheet">
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>Honor Roll Skate Crew</title>
+
+    <link href="/assets/styles.css" rel="stylesheet">
+
+    <title><?php echo TITLE; ?></title>
+
 </head>
 <body>
     <div>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top"><!-- navbar start -->
-            <a class="navbar-brand" href="#">HNRL</a>
+            <a id="hnrl" class="navbar-brand" href="#">HNRL</a>
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,13 +27,13 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item navbar-toggle">
-                        <a class="nav-link" href="/index.php">Home</span></a>
+                        <a class="nav-link" id="navfont1" href="/index.php">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/about.php">About</a>
+                        <a class="nav-link" id="navfont2" href="/about.php">About</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/videos.php">Videos</a>
+                        <a class="nav-link" id="navfont3" href="/videos.php">Videos</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink"
